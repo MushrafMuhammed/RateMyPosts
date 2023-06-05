bf8d9cd83dc4f83b8d611ceb1d34597620fb8700
Mushraf Muhammed <mushrafmuhammed@outlook.com>
2023-06-05 13:48:08 +0530

    models

--- RateMyPosts_api/administrator/models.py
+++ RateMyPosts_api/administrator/models.py
@@ -1,3 +1,19 @@
+from django.db import models
 
+# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    weight = models.IntegerField()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+
+class Post(models.Model):
+    images = models.ManyToManyField('Image')
+    description = models.TextField()
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
 
 
